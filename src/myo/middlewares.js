da2eeb4b52cdfa264353b60d552c537a783d5880
Siyuan Gao <siyuangao@gmail.com>
2016-02-28 04:34:27 -0800

    Change the myos middleware to show number of connected users

--- src/myo/middlewares.js
+++ src/myo/middlewares.js
@@ -11,7 +11,7 @@ function myos(_, res) {
   const TAG = 'myos';
   //  Creating a new shared instance for winston logger
   const sharedInstance = AppSingleton.getInstance();
-  res.send(sharedInstance.myoData);
+  res.send(Object.keys(sharedInstance.myoData).length);
   sharedInstance.L.info(TAG, 'number of myos ' + sharedInstance.myos.length);
 }
 
