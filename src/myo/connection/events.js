0c42aecd7847c85ae1c3852f647b3520b62dac7c
Siyuan Gao <siyuangao@gmail.com>
2016-02-28 07:34:52 -0800

    Adding new clear event

--- src/myo/connection/events.js
+++ src/myo/connection/events.js
@@ -27,6 +27,10 @@ function onDisconnect() {
   const sharedInstance = AppSingleton.getInstance();
   //  Remove the myodata from client
   delete sharedInstance.myoData[this.myoDataId];
+  if (Object.keys(sharedInstance.myoData).length === 0) {
+    sharedInstance.L.info(TAG, 'No Myo Client connected');
+    sharedInstance.myoConsumer.emit('clear');
+  }
   sharedInstance.L.info(TAG, 'Myo Client disconnected');
 }
 
