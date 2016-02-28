5c36a23e6d47f3f591c5a369e7e5ffb9e214fb1e
Siyuan Gao <siyuangao@gmail.com>
2016-02-27 22:43:55 -0800

    Remove the bug for myos

--- src/myo/middlewares.js
+++ src/myo/middlewares.js
@@ -11,7 +11,7 @@ function myos(_, res) {
   const TAG = 'myos';
   //  Creating a new shared instance for winston logger
   const sharedInstance = AppSingleton.getInstance();
-  res.send(sharedInstance.myos);
+  res.send(sharedInstance.myoData);
   sharedInstance.L.info(TAG, 'number of myos ' + sharedInstance.myos.length);
 }
 
